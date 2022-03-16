8ea901cedfdbf590af60f30647dcc0b3c3868ca1
XchuangC JavaScript High Basic <100546975+jshighbasic@users.noreply.github.com>
2022-03-16 16:24:27 +0800

    Add files via upload

--- coding/question/script.js
+++ coding/question/script.js
@@ -1,10 +1,31 @@
+function getNum(n, m){
+    // n, m is a number.
+    // let n have the same number of digits as m.
+    // for example, if n = 1 and m = 100, then n = 001.
+    var str = n.toString();
+    var str2 = m.toString();
+    var len = str2.length;
+    var str3 = '';
+    for (var i = 0; i < len - str.length; i++) {
+        str3 += '0';
+    }
+    str3 += str;
+    return 'Ouput ' + str3;
+}
+
 try {
     location.href.includes('&script=') ? editor.setValue(decodeURIComponent(location.href.split('&script=')[1])) : '';
     location.href.includes('?error=') ? $('#error').html(`<span class="material-icons-round warn">warning</span>&nbsp;${decodeURIComponent(location.href.split('?error=')[1].split('&script=')[0])}`) : '';
     if (location.href.includes('?success=t') && localStorage.getItem(location.href.split('token=')[1].split('&')[0]) == result) {
-        $('#error').html(`<span class="material-icons-round done">done</span>Test passed!<br>Output:<br>${localStorage.getItem(location.href.split('token=')[1].split('&')[0])}`);
+        $('#error').html(`<span class="material-icons-round done">done</span>Test passed!<br>Output<br>==============<br>`);
+        for(let i=0;i<JSON.parse(JSON.parse(localStorage.getItem(location.href.split('token=')[1].split('&')[0])).length);i++){
+            $('#error').append(`${getNum(i+1, JSON.parse(localStorage.getItem(location.href.split('token=')[1].split('&')[0])).length)} | ${JSON.parse(localStorage.getItem(location.href.split('token=')[1].split('&')[0]))[i]}<br>`);
+        }
     } else if (localStorage.getItem(location.href.split('token=')[1].split('&')[0]) != result) {
-        $('#error').html(`<span class="material-icons-round warn">dangerous</span>Wrong answer: <br>Output:<br>${localStorage.getItem(location.href.split('token=')[1].split('&')[0])}`);
+        $('#error').html(`<span class="material-icons-round warn">dangerous</span>Wrong answer: <br>Output:<br>==============<br>`);
+        for(let i=0;i<JSON.parse(JSON.parse(localStorage.getItem(location.href.split('token=')[1].split('&')[0])).length);i++){
+            $('#error').append(`${getNum(i+1, JSON.parse(localStorage.getItem(location.href.split('token=')[1].split('&')[0])).length)} | ${JSON.parse(localStorage.getItem(location.href.split('token=')[1].split('&')[0]))[i]}<br>`);
+        }
     }
 } catch (e) {
     console.log(e);
